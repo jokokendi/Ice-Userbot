96fc2f3d21ef61e816e875dc8a9f2f91bdf5748f
jokokendi <ajual7832@gmail.com>
2022-02-24 17:11:50 +0700

    rename

--- userbot/utils/misc.py
+++ userbot/utils/misc.py
@@ -214,7 +214,7 @@ async def animator(media, mainevent, textevent):
     Kenkan = await mainevent.client.download_media(media, TEMP_DOWNLOAD_DIRECTORY)
     await textevent.edit("`Converting...`")
     await runcmd(
-        f"ffmpeg -ss 00:00:00 -to 00:00:02.900 -i {Risman} -vf scale={w}:{h} -c:v libvpx-vp9 -crf 30 -b:v 560k -maxrate 560k -bufsize 256k -an Video.webm"
+        f"ffmpeg -ss 00:00:00 -to 00:00:02.900 -i {Kenkan} -vf scale={w}:{h} -c:v libvpx-vp9 -crf 30 -b:v 560k -maxrate 560k -bufsize 256k -an Video.webm"
     )
     os.remove(Kenkan)
     vid = "Video.webm"
